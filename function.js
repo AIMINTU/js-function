0caf9737c87335923e2a8fb9799e3acede489b6a
AIMINTU <minto.aminul4@hotmail.com>
2026-02-24 20:51:28 +0100

    Fuction start

---
+++ function.js
@@ -0,0 +1,18 @@
+// function syntax
+
+// fuction declaration
+function fanOffkor(){
+    console.log('bosa theke laf dey')
+    console.log('click the swithch of the fan')
+    console.log('walk towards the stitch')
+}
+
+fanOffkor() // call to the function
+
+function brushteeth(){
+    console.log('Pick up the brush please')
+    console.log('you have do brush twice a day')
+    console.log('You have to buy good quality type of paste')
+}
+//call the fuction
+brushteeth()
